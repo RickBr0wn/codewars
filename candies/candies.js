6755177d53226b628a52ff4dacf85ca1d3985ce2
RiCK BROWN <ricky.brown.00@gmail.com>
2018-04-24 17:12:15 +0100

    Completed candies

--- candies/candies.js
+++ candies/candies.js
@@ -1,7 +1,19 @@
-function candies(n, m) {
+function candies(children, sweets) {
+    let answer = (sweets/children);
 
-    console.log(n, m);
-    return n;
+    if(answer === 1){
+        return children;
+    }
+    
+    if(answer < 1){
+        return 0;
+    }
+    return Math.floor(answer)*children;
 }
 
 candies(3, 10);
+
+// candies(3, 10); - 9
+// candies(1, 2); - 2
+// candies(10, 5); - 0
+// candies(4, 4); - 4
