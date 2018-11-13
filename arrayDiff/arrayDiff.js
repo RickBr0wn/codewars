bb17a49f03b3161513f63e889a019c8b9ac30a16
RickBr0wn <ricky.brown.00@gmail.com>
2018-11-13 22:43:32 +0000

    Completed arrayDiff

--- arrayDiff/arrayDiff.js
+++ arrayDiff/arrayDiff.js
@@ -1,15 +1,1 @@
-const array_diff = (a, b) => { 
-  if(b.length == 0) {
-    return a
-  }else{
-    let newArray = new Array()
-  for(let i = 0; i < b.length; i++) {
-    newArray = a.filter(numA => numA !== b[i])
-  }
-  return newArray
-  }
-}
-
-// array_diff([], [4,5]) //?
-array_diff([3,4], []) //?
-// array_diff([1,8,2], []) //?
+const array_diff = (a, b) => a.filter(num => !b.includes(num))
