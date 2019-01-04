6ee43dec59e618269e5b07a767682347ee5ce75f
RiCK BROWN <ricky.brown.00@gmail.com>
2019-01-04 02:19:21 +0000

    Added TS definition for sumOfTwoIntegers

--- sumOfTwoIntegers/sumOfTwoIntegers.js
+++ sumOfTwoIntegers/sumOfTwoIntegers.js
@@ -1,3 +1,9 @@
-const getSum = (a, b) => new Array(a).fill(0).concat(new Array(b).fill(0)).length
+/**
+* A method to add properties `a` and `b` but without using any mathamatical operators
+* @param `number` - The first number to be added
+* @param `number` - The second number to be added
+* @returns `number` - the sum of the 2 parameters
+*/
+const sumOfTwoIntegers = (a, b) => new Array(a).fill(0).concat(new Array(b).fill(0)).length
 
-getSum(5, 19)
+sumOfTwoIntegers(5, 19)
