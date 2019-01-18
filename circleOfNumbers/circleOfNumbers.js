ac89038489dffd2bcc202caf1c00e1ee63e63841
RiCK BROWN <ricky.brown.00@gmail.com>
2019-01-18 21:05:11 +0000

    Updating algorithm

--- circleOfNumbers/circleOfNumbers.js
+++ circleOfNumbers/circleOfNumbers.js
@@ -1,14 +1,16 @@
 function circleOfNumbers(n, firstNumber) {
-    const createFilledArray = n => {
-        let myArray = Array.apply(null, Array(n))
-        return myArray.map((element, index) => {return index})
-    }
-    let numberArray = createFilledArray(n)
-    let answer = firstNumber + Math.floor(n/2)
-    if(answer > 9){
-        answer -= n;
-    }
-    return answer
+  const createFilledArray = n => {
+    let myArray = Array.apply(null, Array(n))
+    return myArray.map((element, index) => {
+      return index
+    })
+  }
+  let numberArray = createFilledArray(n)
+  let answer = firstNumber + Math.floor(n / 2)
+  if (answer > 9) {
+    answer -= n
+  }
+  return answer
 }
 
 circleOfNumbers(6, 0)
