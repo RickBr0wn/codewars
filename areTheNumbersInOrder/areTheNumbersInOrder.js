8e7061c24adc0c32fabb3b73285d2a924bdf3206
RiCK BROWN <ricky.brown.00@gmail.com>
2019-02-02 23:12:26 +0000

    Updated areTheNumbersInOrder

--- areTheNumbersInOrder/areTheNumbersInOrder.js
+++ areTheNumbersInOrder/areTheNumbersInOrder.js
@@ -17,5 +17,5 @@ const inAscOrder = arr => arr.join('') === arr.sort((a, b) => a - b).join('')
 
 console.log(inAscOrder([1, 2, 4, 7, 19]), true)
 console.log(inAscOrder([1, 2, 3, 4, 5]), true)
-console.log(!inAscOrder([1, 6, 10, 18, 2, 4, 20]), false)
-console.log(!inAscOrder([9, 8, 7, 6, 5, 4, 3, 2, 1]), false)
+console.log(!inAscOrder([1, 6, 10, 18, 2, 4, 20]), true)
+console.log(!inAscOrder([9, 8, 7, 6, 5, 4, 3, 2, 1]), true)
