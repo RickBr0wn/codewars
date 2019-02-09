7836425020c4b284ccbf69a5cb2fbc9498dc69d1
Rick Brown <rickbr0wn@ricks-mbp.home>
2019-02-09 21:36:48 +0000

    playing with jest

---
+++ rowWeights/rowWeights.test.js
@@ -0,0 +1,20 @@
+import { rowWeights } from './rowWeights'
+
+// describe('rowWeights', () => {
+//   Test.assertSimilar(rowWeights([80]), [80, 0])
+//   Test.assertSimilar(rowWeights([100, 50]), [100, 50])
+//   Test.assertSimilar(rowWeights([50, 60, 70, 80]), [120, 140])
+//   Test.assertSimilar(rowWeights([13, 27, 49]), [62, 27])
+//   Test.assertSimilar(rowWeights([70, 58, 75, 34, 91]), [236, 92])
+//   Test.assertSimilar(rowWeights([29, 83, 67, 53, 19, 28, 96]), [211, 164])
+//   Test.assertSimilar(rowWeights([0]), [0, 0])
+//   Test.assertSimilar(rowWeights([100, 51, 50, 100]), [150, 151])
+//   Test.assertSimilar(rowWeights([39, 84, 74, 18, 59, 72, 35, 61]), [207, 235])
+//   Test.assertSimilar(rowWeights([0, 1, 0]), [0, 1])
+// })
+
+describe('rowWeights', () => {
+  it('should return "yo!!!"', () => {
+    expect(rowWeights()).toBe('yo!!!')
+  })
+})
