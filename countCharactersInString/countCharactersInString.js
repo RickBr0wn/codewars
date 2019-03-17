b71a7f2f5464f0111a261409c0aa8ee749109590
Rick Brown <ricky.brown.00@gmail.com>
2019-03-17 19:19:24 +0000

    Added countCharactersInString

---
+++ countCharactersInString/countCharactersInString.js
@@ -0,0 +1,24 @@
+/**
+|--------------------------------------------------
+| The main idea is to count all the occuring characters(UTF-8) in string. 
+| If you have string like this 'aba' then the result should be { 'a': 2, 'b': 1 }
+| What if the string is empty ? 
+| Then the result should be empty object literal { }
+|--------------------------------------------------
+*/
+function count(str) {
+  let table = {}
+
+  str === ''
+    ? table
+    : [...str].map(letter =>
+        Object.keys(table).includes(letter)
+          ? table[letter]++
+          : (table = { ...table, [letter]: 1 })
+      )
+
+  return table
+}
+
+console.log(count('aba'), { a: 2, b: 1 })
+console.log(count(''), {})
