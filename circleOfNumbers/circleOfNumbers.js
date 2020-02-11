846e14b8b6285ae2cd9bc35c32cf965310ae6f6e
Rick Brown <ricky.brown.00@gmail.com>
2020-02-11 01:38:16 +0000

    completes circleOfNumbers

--- circleOfNumbers/circleOfNumbers.js
+++ circleOfNumbers/circleOfNumbers.js
@@ -1,11 +1,3 @@
 module.exports = function circleOfNumbers(n, firstNumber) {
-  const circle = new Array(n).fill(0).map((num, index) => num + index)
-  let counter = 0
-  for (let i = firstNumber; i < Math.floor(circle.length / 2); i++) {
-    if (circle[i + 1] === undefined) {
-      return (counter = 0)
-    }
-    counter = circle[i + 1]
-  }
-  return circle
+  return (n / 2 + firstNumber) % n
 }
