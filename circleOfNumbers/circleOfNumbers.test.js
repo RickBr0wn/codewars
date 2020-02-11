cf75866f761a051ffe1e7cf23ed6f0b9cf06b493
Rick Brown <ricky.brown.00@gmail.com>
2020-02-11 02:59:53 +0000

    corrects desciption

--- circleOfNumbers/circleOfNumbers.test.js
+++ circleOfNumbers/circleOfNumbers.test.js
@@ -1,7 +1,7 @@
 const circleOfNumbers = require("./circleOfNumbers")
 
 describe("circleOfNumbers()", () => {
-  it("should render the correct values", () => {
+  it("should return the correct values", () => {
     expect(circleOfNumbers(10, 2)).toBe(7)
     expect(circleOfNumbers(10, 7)).toBe(2)
     expect(circleOfNumbers(4, 1)).toBe(3)
