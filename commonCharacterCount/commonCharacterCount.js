6509a3988fe947b5349d86a206a70584a9af56e0
Rick Brown <ricky.brown.00@gmail.com>
2020-02-13 11:51:24 +0000

    adds commonCharacterCount

---
+++ commonCharacterCount/commonCharacterCount.js
@@ -0,0 +1,36 @@
+/**
+Given two strings, find the number of common characters between them.
+
+Example:
+For s1 = "aabcc" and s2 = "adcaa", the output should be
+commonCharacterCount(s1, s2) = 3.
+Strings have 3 common characters - 2 "a"s and 1 "c".
+
+Input/Output:
+[execution time limit] 4 seconds (js)
+[input] string s1
+A string consisting of lowercase English letters.
+Guaranteed constraints:
+1 ≤ s1.length < 15.
+[input] string s2
+A string consisting of lowercase English letters.
+Guaranteed constraints:
+1 ≤ s2.length < 15.
+[output] integer
+ */
+module.exports = function commonCharacterCount(s1, s2) {
+  let arr2 = s2.split("")
+  let counter = 0
+
+  for (const firstLetter of s1.split("")) {
+    for (const secondLetter of arr2) {
+      if (firstLetter === secondLetter) {
+        arr2.splice(arr2.indexOf(secondLetter), 1)
+        counter++
+        break
+      }
+    }
+  }
+
+  return counter
+}
---
+++ commonCharacterCount/commonCharacterCount.test.js
@@ -0,0 +1,11 @@
+const commonCharacterCount = require("./commonCharacterCount.js")
+
+describe("commonCharacterCount()", () => {
+  it("should return the correct number of common characters", () => {
+    expect(commonCharacterCount("aabcc", "adcaa")).toBe(3)
+    expect(commonCharacterCount("zzzz", "zzzzzzz")).toBe(4)
+    expect(commonCharacterCount("abca", "xyzbac")).toBe(3)
+    expect(commonCharacterCount("a", "b")).toBe(0)
+    expect(commonCharacterCount("a", "aaa")).toBe(1)
+  })
+})
