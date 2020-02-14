3817424184bdb1f0366ce084d8a5c64e2c4aee54
Rick Brown <ricky.brown.00@gmail.com>
2020-02-14 11:21:35 +0000

    adds newYearCelebrations

---
+++ newYearCelebrations/newYearCelebrations.test.js
@@ -0,0 +1,69 @@
+const newYearCelebrations = require("./newYearCelebrations")
+
+describe("newYearCelebrations()", () => {
+  it("should ", () => {
+    // expect(newYearCelebrations("23:35", [60, 90, 140])).toBe(3)
+    // expect(newYearCelebrations("00:00", [60, 120, 180, 250])).toBe(4)
+    // expect(newYearCelebrations("13:36", [23, 33, 45, 56, 66, 88])).toBe(3)
+    // expect(newYearCelebrations("22:50", [])).toBe(1)
+    // expect(newYearCelebrations("20:18", [222, 342])).toBe(3)
+    // expect(
+    //   newYearCelebrations("12:05", [
+    //     1,
+    //     109,
+    //     113,
+    //     344,
+    //     345,
+    //     478,
+    //     545,
+    //     565,
+    //     809,
+    //     814,
+    //     838,
+    //     883,
+    //     971,
+    //     1007,
+    //     1029,
+    //     1053,
+    //     1133,
+    //     1271,
+    //     1314,
+    //     1500
+    //   ])
+    // ).toBe(3)
+    // expect(
+    //   newYearCelebrations("17:10", [
+    //     150,
+    //     160,
+    //     293,
+    //     589,
+    //     614,
+    //     716,
+    //     760,
+    //     776,
+    //     781,
+    //     911,
+    //     1040,
+    //     1438
+    //   ])
+    // ).toBe(3)
+    // expect(newYearCelebrations("18:15", [117, 241, 364, 375, 545, 1317])).toBe(
+    //   3
+    // )
+    // expect(newYearCelebrations("19:44", [545, 1320])).toBe(3)
+    // expect(
+    //   newYearCelebrations("21:13", [
+    //     52,
+    //     257,
+    //     323,
+    //     515,
+    //     579,
+    //     600,
+    //     703,
+    //     707,
+    //     1127,
+    //     1298
+    //   ])
+    // ).toBe(3)
+  })
+})
