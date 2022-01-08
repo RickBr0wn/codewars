09ea0d2170b711eedafb8467f90e214ae3c7ab7b
Rick Brown <ricky.brown.00@gmail.com>
2022-01-08 16:24:33 +0000

    cleans and tests convertColorImageToGreyscale**

--- **convertColorImageToGreyscale/convertColorImageToGreyscale.js
+++ **convertColorImageToGreyscale/convertColorImageToGreyscale.js
@@ -1,13 +1,39 @@
-var color2grey = function(image) {
-  let greyscale = null
-
-  return image.map((outerArray, outerArrayIndex) =>
-    outerArray.map((innerArray, innerArrayIndex) => {
-      const averageNumber = Math.floor(
-        innerArray.reduce((acc, num) => acc + num, 0) / 3
+/**
+ * CONVERT COLOR IMAGE TO GREYSCALE
+ *
+ * An array of size N x M represents pixels of an image.
+ * Each cell of this array contains an array of size 3 with the
+ * pixel's color information: [R,G,B].
+ * Convert the color image, into an average greyscale image.
+ * The [R,G,B] array contains integers between 0 and 255 for each color.
+ *
+ * To transform a color pixel into a greyscale pixel, average the values of that pixel:
+ * p = [R,G,B] => [(R+G+B)/3, (R+G+B)/3, (R+G+B)/3]
+ * Note: the values for the pixel must be integers, therefore you should round floats to the nearest integer.
+ *
+ * Example:
+ * Here's an example of a 2x2 image:
+ * [
+ *   [ [123, 231, 12], [56, 43, 124] ],
+ *   [ [78, 152, 76], [64, 132, 200] ]
+ * ]
+ * Here's the expected image after transformation:
+ * [
+ *   [ [122, 122, 122], [74, 74, 74] ],
+ *   [ [102, 102, 102], [132, 132, 132] ]
+ * ]
+ */
+var color2grey = function (image) {
+  return image.map(outer =>
+    outer.map(inner => {
+      const valueToBeReturned = Math.floor(
+        inner.reduce((acc, val) => acc + val) / 3
       )
-
-      return averageNumber
+      let returnArray = []
+      for (let i = 0; i < inner.length; i++) {
+        returnArray.push(valueToBeReturned)
+      }
+      return returnArray
     })
   )
 }
