87e72a8b0654ae7244e345b62cb807b9342bb240
Rick Brown <ricky.brown.00@gmail.com>
2022-01-12 18:13:55 +0000

    completes centuryFromYear

---
+++ CodeSignal/02 - centuryFromYear/centuryFromYear.test.js
@@ -0,0 +1,15 @@
+const centuryFromYear = require('./centuryFromYear')
+
+describe('centuryFromYear (fn)', () => {
+  it('should pass all basic tests', () => {
+    expect(centuryFromYear(1905)).toBe(20)
+    expect(centuryFromYear(1700)).toBe(17)
+    expect(centuryFromYear(1988)).toBe(20)
+    expect(centuryFromYear(2000)).toBe(20)
+    expect(centuryFromYear(2001)).toBe(21)
+    expect(centuryFromYear(200)).toBe(2)
+    expect(centuryFromYear(374)).toBe(4)
+    expect(centuryFromYear(45)).toBe(1)
+    expect(centuryFromYear(8)).toBe(1)
+  })
+})
