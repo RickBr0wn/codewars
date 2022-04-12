54d410ed4644afda1dcbab3329dd240a1e7337a8
Rick Brown <ricky.brown.00@gmail.com>
2022-04-12 14:45:11 +0100

    adds searchInsert

---
+++ __LeetCode/035_searchInsertPosition/searchInsertPosition.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+module.exports = function searchInsert(nums, target) {
+  let left = 0
+  let right = nums.length - 1
+
+  while (left <= right) {
+    const center = Math.floor((right - left) / 2) + left
+
+    if (target == nums[center]) {
+      return center
+    }
+
+    if (target < nums[center]) {
+      right = center - 1
+    } else {
+      left = center + 1
+    }
+  }
+
+  return left
+}
+// searchInsert([1,3,5,6], 5) -> 2
