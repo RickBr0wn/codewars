90cbf01dcdf9f6352cd3f4dbb9d6229b15b08cdc
Rick Brown <ricky.brown.00@gmail.com>
2022-04-12 14:05:02 +0100

    adds guessNumberHigherOrLower

--- __LeetCode/374_guessNumberHigherOrLower/guessNumberHigherOrLower.js
+++ __LeetCode/374_guessNumberHigherOrLower/guessNumberHigherOrLower.js
@@ -13,22 +13,22 @@ function guess(num) {
   }
 }
 
-module.exports = function guessNumber(n, start = 1, end = n) {
+module.exports = function guessNumberHigherOrLower(n, start = 1, end = n) {
+  if (n === 1 || n === 2) {
+    return 1
+  }
+
   if (guess(start) === 0) {
     return start
   }
 
   let middle = start + Math.floor((end - start) / 2)
-  console.log(`middle: ${middle}`)
 
   if (guess(middle) === 0) {
-    console.log(`guess returns zero: ${guess(middle)}`)
     return middle
   } else if (guess(middle) === -1) {
-    console.log(`guess returns minus one: ${guess(middle)}`)
-    return guessNumber(n, start, middle)
+    return guessNumberHigherOrLower(n, start, middle)
   } else if (guess(middle) === 1) {
-    console.log(`guess returns one: ${guess(middle)}`)
-    return guessNumber(n, middle + 1, end)
+    return guessNumberHigherOrLower(n, middle + 1, end)
   }
 }
