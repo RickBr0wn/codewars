f5a74687bee418731d73543d17b46081182b19ca
Rick Brown <ricky.brown.00@gmail.com>
2022-06-22 14:59:48 +0100

    edit only: test description

--- __LeetCode/1 - twoSum/twoSum.test.js
+++ __LeetCode/1 - twoSum/twoSum.test.js
@@ -1,6 +1,6 @@
 const twoSum = require('./twoSum')
 
-describe('searchInsertPosition (fn)', () => {
+describe('twoSum (fn)', () => {
   it('should pass all basic tests', () => {
     expect(twoSum([2, 7, 11, 15], 9)).toStrictEqual([0, 1])
     expect(twoSum([3, 2, 4], 6)).toStrictEqual([1, 2])
