d145ca47115c75ff19f3fc6689748af6e8a2932c
Rick Brown <ricky.brown.00@gmail.com>
2022-06-23 11:17:03 +0100

    converts to class

--- __DataStructures/HashTable.js
+++ __DataStructures/HashTable.js
@@ -6,53 +6,55 @@ var hash = (string, max) => {
   return hash % max
 }
 
-let HashTable = function() {
-  let storage = []
-  const storageLimit = 14
+class HashTable {
+  constructor() {
+    let storage = []
+    const storageLimit = 14
 
-  this.print = function() {
-    console.log(storage)
-  }
-
-  this.add = function(key, value) {
-    var index = hash(key, storageLimit)
-    if (storage[index] === undefined) {
-      storage[index] = [[key, value]]
-    } else {
-      var inserted = false
-      for (var i = 0; i < storage[index].length; i++) {
-        if (storage[index][i][0] === key) {
-          storage[index][i][1] = value
-          inserted = true
-        }
-      }
-      if (inserted === false) {
-        storage[index].push([key, value])
-      }
+    this.print = function () {
+      console.log(storage)
     }
-  }
 
-  this.remove = function(key) {
-    var index = hash(key, storageLimit)
-    if (storage[index].length === 1 && storage[index][0][0] === key) {
-      delete storage[index]
-    } else {
-      for (var i = 0; i < storage[index].length; i++) {
-        if (storage[index][i][0] === key) {
-          delete storage[index][i]
+    this.add = function (key, value) {
+      var index = hash(key, storageLimit)
+      if (storage[index] === undefined) {
+        storage[index] = [[key, value]]
+      } else {
+        var inserted = false
+        for (var i = 0; i < storage[index].length; i++) {
+          if (storage[index][i][0] === key) {
+            storage[index][i][1] = value
+            inserted = true
+          }
+        }
+        if (inserted === false) {
+          storage[index].push([key, value])
         }
       }
     }
-  }
 
-  this.lookup = function(key) {
-    var index = hash(key, storageLimit)
-    if (storage[index] === undefined) {
-      return undefined
-    } else {
-      for (var i = 0; i < storage[index].length; i++) {
-        if (storage[index][i][0] === key) {
-          return storage[index][i][1]
+    this.remove = function (key) {
+      var index = hash(key, storageLimit)
+      if (storage[index].length === 1 && storage[index][0][0] === key) {
+        delete storage[index]
+      } else {
+        for (var i = 0; i < storage[index].length; i++) {
+          if (storage[index][i][0] === key) {
+            delete storage[index][i]
+          }
+        }
+      }
+    }
+
+    this.lookup = function (key) {
+      var index = hash(key, storageLimit)
+      if (storage[index] === undefined) {
+        return undefined
+      } else {
+        for (var i = 0; i < storage[index].length; i++) {
+          if (storage[index][i][0] === key) {
+            return storage[index][i][1]
+          }
         }
       }
     }
@@ -68,8 +70,8 @@ ht.add('rex', 'dinosour') //?
 ht.add('tux', {
   name: 'test',
   address: {
-    town: 'ashford'
-  }
+    town: 'ashford',
+  },
 }) //?
 console.log(ht.lookup('tux')) //?
 ht.print() //?
