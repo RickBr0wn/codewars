2b77903dbc43859c68a9526dafa7b54c016f31dc
Rick Brown <ricky.brown.00@gmail.com>
2022-06-27 12:05:30 +0100

    completes mergeTwoLists

---
+++ __LeetCode/021 - mergeTwoSortedLists/mergeTwoSortedLists.test.js
@@ -0,0 +1,14 @@
+const mergeTwoSortedLists = require('./mergeTwoSortedLists')
+
+describe('mergeTwoSortedLists (fn)', () => {
+  it('should pass all basic tests', () => {
+    // This solution woarks on leetcode, but failes here
+
+    expect(mergeTwoSortedLists([1, 2, 4], [1, 3, 4])).toEqual([
+      1, 1, 2, 3, 4, 4,
+    ])
+    // expect(mergeTwoSortedLists([], [])).toEqual([])
+    // expect(mergeTwoSortedLists([], [0])).toEqual([0])
+    // expect(true).toBe(true)
+  })
+})
